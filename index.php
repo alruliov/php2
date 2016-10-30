6caad5afa5a2bb039cdec9eb87f30e4cc20e20bb
Alexandro Ruliov <al.ruliov@gmail.com>
2016-10-30 14:12:33 +0300

    Заметил ошибку

--- index.php
+++ index.php
@@ -18,14 +18,5 @@ $controller->action($ctrlRequest, $actionRequest);
 
 
 
-$url = $_SERVER['REQUEST_URI'];
-
-$parts = explode('/', $url);
-
-$ctrlRequest = !empty($parts[1]) ? $parts[1] : 'Index';
-
-$actionRequest = !empty($parts[2]) ? $parts[2] : 'Default';
-
-
 
 
