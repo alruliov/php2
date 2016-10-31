07c6ef2a9de01f12a5f99f3095bdc42229f50e4f
Alexandro Ruliov <al.ruliov@gmail.com>
2016-10-31 21:05:31 +0300

    ДЗ 5 - перенес исключение в в контроллер

--- App/Controllers/News.php
+++ App/Controllers/News.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 
 use App\Controller;
+use App\CustomException;
 use App\Model\Article;
 use App\View;
 
@@ -14,13 +15,27 @@ class News extends Controller
 
     public function actionGetLimit()
     {
-        $this->view->articles = Article::findAllByLimit(3);
+        $articles = Article::findAllByLimit(3);
+        if (empty($articles)){
+
+            $e = new CustomException('По вашему запросу ничего не найдено');
+            throw $e;
+
+        }
+        $this->view->articles = $articles;
         $this->view->display(__DIR__ . '/../Template/article.php');
     }
 
     public function actionGetOne()
     {
-        $this->view->article = Article::findById($_GET['id']);
+        $article = Article::findById($_GET['id']);
+        if (empty($article)){
+
+            $e = new CustomException('По вашему запросу ничего не найдено');
+            throw $e;
+
+        }
+        $this->view->article = $article;
         $this->view->display(__DIR__ . '/../Template/articleOne.php');
     }
 
